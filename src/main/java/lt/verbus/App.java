8ef54812216048c1f5a8b726fe8e0f30bc0b5d0d
verbusharas <sarunas.verbus@gmail.com>
2020-09-30 15:31:00 +0300

    Created Student POJO and session factory to load configuration. Minimal code for table creation

--- src/main/java/lt/verbus/App.java
+++ src/main/java/lt/verbus/App.java
@@ -1,13 +1,15 @@
 package lt.verbus;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+//        Session session = sessionFactory.openSession();
+//        session.close();
     }
 }
---
+++ src/main/java/lt/verbus/model/Student.java
@@ -0,0 +1,50 @@
+package lt.verbus.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer result;
+
+    public Student(){
+
+    }
+
+    public Student(Long id, String name, Integer result) {
+        this.id = id;
+        this.name = name;
+        this.result = result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+}
